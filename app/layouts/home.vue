1461818274ae10a32c69292eb2cbeee3c5853f13
Aleksa Sovrlic <aleksasovrlic@gmail.com>
2026-01-19 23:06:44 +0100

    index page redesign fix

--- app/layouts/home.vue
+++ app/layouts/home.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Absolute + transparent, NOT sticky; must NOT reserve layout height -->
-  <PublicHeader class="absolute top-0 inset-x-0 z-10" variant="transparent" maxWidth="6xl" />
+  <PublicHeader class="absolute top-0 inset-x-0 z-50" variant="transparent" maxWidth="6xl" />
   <slot />
 </template>
 
